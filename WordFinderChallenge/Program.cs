2dff3aa95963de73cd181144fd5eeb885ad93fc5
Alfredo Soler <alfredoj.solerp@gmail.com>
2024-10-26 17:44:21 -0300

    First commit

---
+++ WordFinderChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ *	My reasoning was as follows:
+
+	The exercise was all about an alphabet soup, and this implies one thing: you have to do a linear search. There's no way to escape from that. You have to search for words 
+	vertically or horizontally. So how can you do this quickly? Well, a better question would be: do I have to search row by row and column by column? Or can I search all rows and columns at the
+	same time? Once I asked myself the question the answer came naturally to my head: asynchronous code.
+
+	And that was it, that was my reasoning :D
+ */
+
+// 1. Usings.
+using System.Diagnostics;
+using WordFinderChallenge;
+
+// 2. Declarations.
+IEnumerable<string> wordstream = [];    // Insert your wordstream here.
+IEnumerable<string> matrix = [];		// Insert your matrix here.
+var timer = new Stopwatch();            // Timer to evaluate which approximation is the most efficient (List, HashSet, with async, without async, etc).
+
+// 3. Main program.
+timer.Start();
+wordstream = ["toast", "yellow", "backend", "frontend", "engineer", "keyboard", "programmer", "friday", "amen", "mortal"];
+matrix = ["agmqexeetpj", "frontendorh", "rbriergyaok", "istcrjizsgb", "dtaandnktra", "aulbmfeemac", "yellowegfmk", "nvchzmrlbme", "wodihinamen", "keyboardprd"];
+var wordFinder = new WordFinder(matrix);
+
+var result = wordFinder.Find(wordstream);
+timer.Stop();
+
+// 4. Show the results.
+Console.WriteLine($"Top 10 results");
+Console.WriteLine("------------------------------");
+
+foreach (var word in result)
+{
+	Console.WriteLine($"> {word}.");
+}
+
+Console.WriteLine("------------------------------");
+Console.WriteLine($"> Matrix count: {matrix.Count()}");
+// I'm wondering if the block of the main thread affects the timer.
+Console.WriteLine($"> Total milliseconds: {timer.Elapsed.TotalMilliseconds}");
+Console.WriteLine("------------------------------");
+Console.WriteLine("I hope you find all that you are searching for in this code :)");
---
+++ WordFinderChallenge/WordFinder.cs
@@ -0,0 +1,96 @@
+﻿namespace WordFinderChallenge
+{
+	internal class WordFinder
+	{
+		private List<string> _matrix;
+
+		public WordFinder(IEnumerable<string> matrix)
+		{
+			_matrix = new List<string>(matrix);
+		}
+
+		public IEnumerable<string> Find(IEnumerable<string> wordstream)
+		{
+			// Let's throw some multitask work to get job done faster.
+			var searchingTasks = new List<Task<IEnumerable<string>>>
+			{
+				SearchHorizontally(wordstream),
+				SearchVertically(wordstream)
+			};
+
+			// Run tasks and capture results.
+			// We can't change the firm of the method, so we need to use 'Result' instead of 'await'.
+			// That is dangerous since it blocks the main thread, but is blocking the UI a concern? For this challenge, no.
+			var searchResults = Task.WhenAll(searchingTasks).Result;			
+
+			// Results are no unique here, so we need to filter the top 10 most repeated words.
+			// Linq is good for stuff like this.
+			var uniqueResults = searchResults
+				.SelectMany(ienumerable => ienumerable) // The Task.WhenAll returns an array of IEnumerables, so lets flatten that first
+				.GroupBy(word => word)
+				.OrderByDescending(group => group.Count())
+				.Take(10)
+				.Select(group => group.Key);
+
+			return uniqueResults ?? [];
+		}
+
+		// Method to search words from left to right.
+		private async Task<IEnumerable<string>> SearchHorizontally(IEnumerable<string> wordstream)
+		{
+			var foundWords = new List<string>();
+
+			// Run parallel searches for each row to maximize the speed.
+			await Task.WhenAll(_matrix.Select(async row =>
+			{
+				foreach (var word in wordstream)
+				{
+					if (row.Contains(word))
+					{
+						// Avoid collisioning between threads by locking the list.
+						lock (foundWords)
+						{
+							foundWords.Add(word);
+						}
+					}
+				}
+			}));
+
+			return foundWords;
+		}
+
+		// Method to search words from top to bottom.
+		private async Task<IEnumerable<string>> SearchVertically(IEnumerable<string> wordstream)
+		{
+			var foundWords = new List<string>();
+
+			// For vertical search we need to first create the columns.
+			// We can assume that all words have the same length because the exercise told us.
+			var columns = new List<string>();
+
+			// A column is created selecting the character in the X position of the N rows.
+			for (int x = 0; x < _matrix.First().Length; x++)
+			{
+				columns.Add(new string(_matrix.Select(row => row[x]).ToArray()));
+			}
+
+			// Run parallel searches for each column to maximize the speed.
+			await Task.WhenAll(columns.Select(async column =>
+			{
+				foreach (var word in wordstream)
+				{
+					if (column.Contains(word))
+					{
+						// Avoid collisioning between threads by locking the list.
+						lock (foundWords)
+						{
+							foundWords.Add(word);
+						}
+					}
+				}
+			}));
+
+			return foundWords;
+		}
+	}
+}
